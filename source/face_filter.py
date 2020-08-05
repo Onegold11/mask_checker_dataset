b20383e169d99672388f3a8f5f554d5527ed5e2a
Onegold11 <43362141+Onegold11@users.noreply.github.com>
2020-08-05 11:55:30 +0900

    Resize image
    + image resize

--- source/face_filter.py
+++ source/face_filter.py
@@ -1,6 +1,11 @@
 import os
 import cv2
 
+type = "no_mask"
+source_path = "../resize/" + type + "/"
+result_path = "../result/" + type + "/"
+
+
 def make_image_directory(name):
     dir_path = "../images/" + name
     if not os.path.exists(dir_path):
@@ -8,14 +13,14 @@ def make_image_directory(name):
 
 
 def get_face():
-    #make_image_directory("mask_converted")
-    #detect_faces("../new_image/")
-    detect_faces("../images/mask/")
+    # make_image_directory("mask_converted")
+    detect_faces(source_path)
 
 
 def detect_faces(dir_path):
     file_list = os.listdir(dir_path)
 
+    num = 0
     for image_name in file_list:
         face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
@@ -24,16 +29,26 @@ def detect_faces(dir_path):
         faces = face_cascade.detectMultiScale(gray)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cropped = img[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
 
-        cv2.imshow("Image view", img)
+            print(len(cropped))
+            try:
+                cv2.imwrite(result_path + type + "_" + str(num) + ".png", cropped)
+            except:
+                print("오류")
+            print(result_path + type + "_" + str(num) + ".png" + " complete")
+            num += 1
 
-        input = cv2.waitKey(0)
-        if input == 120 or input == 88:
-            cv2.destroyAllWindows()
-            break
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        cv2.destroyAllWindows()
+        # cv2.imshow("Image view", img)
+
+        # input = cv2.waitKey(0)
+        # if input == 120 or input == 88:
+        # cv2.destroyAllWindows()
+        # break
+
+        # cv2.destroyAllWindows()
         # image = cv2.imread(dir_path + image_name)
         # faces, confidences = cv.detect_face(image)
         #
